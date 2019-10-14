416a220e82270b82519f5ac55142c4c67167975e
koso00 <danilo.flemma@welan.it>
2019-10-14 11:21:53 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__. '/vendor/autoload.php';
+
+
+
+$username = getenv("USERNAME");
+$password = getenv("PASSWORD");
+$startId = intval(getenv("START"));
+$endId = intval(getenv("END"));
+
+if ($endId <= $startID){
+    throw new \Exception("End must not be minor than start");
+}
+//("percosets","Jewell4Life691029");
+//setProxy( $socialclub);
+$socialclub = new Nve\SocialClub\Client(); 
+$i = $startId;
+$socialclub->users->login($username,$password);
+while($i < $endId){
+    //$socialclub->signup("danisdnsaskd@".generateRandomString().".com",generateRandomString(),generateRandomString(),generateRandomString());
+    $socialclub->users->follow($i);
+    //sleep(0.5);
+    $i++;
+}
